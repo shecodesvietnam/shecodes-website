3782b71c2de04a2e13fb9dc9e0c6bdaee2544841
Vu Huy <vuhuycto@gmail.com>
2020-08-03 23:49:10 +0700

    Updates

---
+++ precache-manifest.52e8b840b3b7a8fa9f4a20e573e3616f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5fac6d29067d350dee513ce13511bc7",
+    "url": "/shecodes-website-2020-aws/index.html"
+  },
+  {
+    "revision": "33edb31a9b8b1b301168",
+    "url": "/shecodes-website-2020-aws/static/css/main.4053de78.chunk.css"
+  },
+  {
+    "revision": "3ba5458c4e3ab7c8e9ef",
+    "url": "/shecodes-website-2020-aws/static/js/2.6444f532.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/shecodes-website-2020-aws/static/js/2.6444f532.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33edb31a9b8b1b301168",
+    "url": "/shecodes-website-2020-aws/static/js/main.caf47d0b.chunk.js"
+  },
+  {
+    "revision": "1da6c4a2ba9ac2352f55",
+    "url": "/shecodes-website-2020-aws/static/js/runtime-main.b8f74f80.js"
+  },
+  {
+    "revision": "177764eadf43efc393ebb98329a774b2",
+    "url": "/shecodes-website-2020-aws/static/media/linea-basic-10.177764ea.svg"
+  },
+  {
+    "revision": "61f3a8a387a64abfc0535cd0813cd71a",
+    "url": "/shecodes-website-2020-aws/static/media/linea-basic-10.61f3a8a3.woff"
+  },
+  {
+    "revision": "e4de528c00da0c081906c3caea289f0f",
+    "url": "/shecodes-website-2020-aws/static/media/linea-basic-10.e4de528c.eot"
+  },
+  {
+    "revision": "e6763f008205c55db623cdf68bbd581f",
+    "url": "/shecodes-website-2020-aws/static/media/linea-basic-10.e6763f00.ttf"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/shecodes-website-2020-aws/precache-manifest.52e8b840b3b7a8fa9f4a20e573e3616f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/shecodes-website-2020-aws/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});

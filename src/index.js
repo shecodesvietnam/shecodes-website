65c630e59fab9b87ed97ef070bcb3f07207a4205
Vu Huy <vuhuycto@gmail.com>
2020-08-27 23:15:22 +0700

    modify react router for aws

--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 
 import ScrollToTop from './ScrollToTop';
+
+const history = createBrowserHistory();
+
+const path = (/#!(\/.*)$/.exec(document.location.hash) || [])[1];
+if (path) {
+	history.replace(path);
+}
 
 ReactDOM.render(
 	<React.StrictMode>
